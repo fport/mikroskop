ea766c4d30478a7b17579edddea1e2c7ad6fdcf7
fport <furkanportakalx@gmail.com>
2022-11-04 16:20:52 +0300

    init

---
+++ home/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.scss";
+
+import Header from "./Header";
+import Footer from "./Footer";
+
+const App = () => (
+  <div className="text-3xl mx-auto max-w-6xl">
+    <Header />
+    <div className="my-10">
+      Home Page Content
+    </div>
+    <Footer />
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ pdp/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.scss";
+
+import Header from "home/Header";
+import Footer from "home/Footer";
+
+const App = () => (
+  <div className="text-3xl mx-auto max-w-6xl">
+    <Header />
+    <div className="my-10">
+      PDP Page Content
+    </div>
+    <Footer />
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
